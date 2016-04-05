56b3f0a8fea0fe4977684329ea291f7bd5eff437
Chris Parker <cjp214@gmail.com>
2016-04-05 10:03:27 +0100

    merging branch 2 into master

---
+++ spec/features/list_spaces_spec.rb
@@ -0,0 +1,14 @@
+feature 'list spaces' do
+
+  scenario 'I can add a name, description and price to my space' do
+    visit '/new/space'
+    fill_in :name, with: "chris's space"
+    fill_in :description, with: "a lovely calming space"
+    fill_in :price, with: "£20 per night"
+    click_button('submit')
+    expect(page).to have_content "chris's space"
+    expect(page).to have_content "a lovely calming space"
+    expect(page).to have_content "£20 per night"
+  end
+
+end
---
+++ todolist.md
@@ -0,0 +1,3 @@
+User stories created as issues
+Project setup/plan
+Start work on user stories
