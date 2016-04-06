75fb1fdda0b7551ea8c9ef0fff06fd4483fb2819
Chris Parker <cjp214@gmail.com>
2016-04-06 11:39:58 +0100

    spaces belong to user accounts

--- app/controllers/user_spaces.rb
+++ app/controllers/user_spaces.rb
@@ -1,8 +1,8 @@
 class MakersBNB < Sinatra::Base
 
   get '/user_spaces' do
-    @spaces = @current_user.spaces.all
-    erb :'spaces/spaces'
+    @spaces = current_user.spaces
+    erb :'spaces/my_spaces'
   end
 
 end
